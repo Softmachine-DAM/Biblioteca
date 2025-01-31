4ad5c44051d18f915f1ea78fd9b7d8fb91496051
CrisGreenfield <cristobal.greenfield03@somo.eus>
2025-01-31 08:15:01 +0100

    arreglar link estilos (VerQuejas.php)

--- es/VerQuejas.php
+++ es/VerQuejas.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ver Quejas</title>
-    <link rel="stylesheet" href="../Styles/VerQuejas.css>
+    <link rel="stylesheet" href="../Styles/VerQuejas.css">
 </head>
 <body>
 <div class="container-table">

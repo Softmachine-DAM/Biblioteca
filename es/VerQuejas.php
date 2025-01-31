bb45e71fcbaed7a2dd4c4b133f89fa65a57a2500
Biriki22 <markel.neila.2004@gmail.com>
2025-01-31 09:30:46 +0100

    .

--- es/VerQuejas.php
+++ es/VerQuejas.php
@@ -11,6 +11,26 @@ $usuarios = "SELECT * FROM quejas";
     <link rel="stylesheet" href="../Styles/VerQuejas.css">
 </head>
 <body>
+<header>
+        <div class="logo">
+            <img src="../IMAGENES/LOGO_MUSKIZ.jpg" alt="Bilbao Logo">
+            <h1 class="Biblioteca_muskiz">   BIBLIOTECA DE MUSKIZ</h1>
+        </div>
+        <nav class="menu">
+            <a href="Index.html"> |    <b>Inicio</b>     |</a>
+            <a href="Contacto.php">|    <b>Ayuda</b>    |</a>
+            <a href="Accesibilidad.html">|  <b>Accesibilidad</b>    |</a>
+            <a href="Registro.html">  <b>| Registro  |</b>   </a>
+            <a href="../eus/Index(eus).html">  <b> >  Euskaraz  </b>   </a>
+           
+        </nav>
+        <div class="contacto">
+            <a href="softmachines@gmial.com"> 
+                
+            </a>
+
+        </div>
+    </header>
 <div class="container-table">
         <div class="table__title">Contacto</div>
         <div class="table__header">Nombre</div>
@@ -27,5 +47,29 @@ $usuarios = "SELECT * FROM quejas";
 
 
             <sub><a href="../es/contacto.php" style="text-decoration-color: blue;">Pulsa aqui para volver a la pagina de contacto</a></sub>  
+    </div>
 </body>
+<footer class="footer">
+        <div class="footer-section footer-links">
+          <a></a>
+            <a href="#">Política de privacidad</a>
+          <span>|</span>
+          <a href="somorrostro.com">Aviso legal</a>
+        </div>
+        <div class="footer-section footer-info">
+          Ayuntamiento de Bilbao. Área de Cultura y Educación. Subdirección de Bibliotecas<br>
+          Calle Bidebarrieta, nº4, 48005 Bilbao.<br>
+          Teléfonos: 944 150 915, 944 156 930
+        </div>
+        <div class="footer-section footer-social">
+          <a href="https://www.facebook.com/muskizko.liburutegia/"><img src="facebook-icon.png" alt="Facebook"></a>
+          <a href="@Muskiz.x"><img src="twitter-icon.png" alt="Twitter"></a>
+          <a href="https://www.instagram.com/muskizudala/"><img src="/IMAGENES/Instagram_logo_2022.svg.png" alt="Instagram"></a>
+          <a href="#"><img src="youtube-icon.png" alt="YouTube"></a>
+        </div>
+        <div class="Softmachines">
+          <small>&copy;2024 <small>Softmachines </small> - Todos los Derechos Reservados.</small>
+          </div>
+ 
+      </footer> 
 </html>
